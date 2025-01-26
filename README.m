83fe07da87692b801ac4123ccbe604038a0c037a
Selincevk <selincevk@gmail.com>
2025-01-26 22:16:19 +0300

    readme

--- README.m
+++ README.m
@@ -1,1 +1,17 @@
-# QrMen-
+<h1> QR Menü 📱🍽️  </h1>
+
+QR Menü, restoran ve kafeler için dijital bir menü çözümü sunan bir uygulamadır. Proje, db.json dosyasını kullanarak menü verilerini yönetir.Bu uygulama basit HTML, CSS ve JavaScript ile geliştirilmiştir.
+
+<h1>Özellikler</h1>
+
+Menü Elemanlarını Render Etme: fetchMenu fonksiyonu ile APIden alınan veri ile menü kartları dinamik olarak oluşturulur. 🖼️
+
+Filtreleme: Kullanıcı, menüdeki kategoriler arasında seçim yaparak, sadece seçili kategoriye ait ürünleri görebilir. 🎯
+
+Ürün Detay Sayfası: Kullanıcı bir ürüne tıkladığında, ürünün detay sayfasına yönlendirilir. Bu sayfada, ürünün açıklaması, fiyatı ve kategorisi gibi bilgiler gösterilir. 
+
+db.json 📑 Veri kaynağımız olan JSON dosyasının yapısı burada bulunur. Bu dosya, ürünlerin adları, fiyatları, açıklamaları ve görselleri gibi bilgileri içerir.
+
+<h1>DEMO</h1>
+
+![]()
